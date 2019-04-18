3c6bf2712fe6c30d416d3b97a6a839bc5c3ee83c
Felix Parker <flixpar@gmail.com>
2019-04-17 20:24:59 -0400

    added stdout blocker

--- alphatsp/util.py
+++ alphatsp/util.py
@@ -2,6 +2,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 plt.style.use("seaborn-darkgrid")
 
+import os, sys
+from contextlib import contextmanager
+
 def display_tour(tsp, tour):
 	points = tsp.points
 	points = points[tour]
@@ -10,3 +13,18 @@ def display_tour(tsp, tour):
 	plt.xlim(0,1)
 	plt.ylim(0,1)
 	plt.show()
+
+@contextmanager
+def stdout_redirected(to=os.devnull):
+	fd = sys.stdout.fileno()
+	def _redirect_stdout(to):
+		sys.stdout.close()
+		os.dup2(to.fileno(), fd)
+		sys.stdout = os.fdopen(fd, 'w')
+	with os.fdopen(os.dup(fd), 'w') as old_stdout:
+		with open(to, 'w') as file:
+			_redirect_stdout(to=file)
+		try:
+			yield
+		finally:
+			_redirect_stdout(to=old_stdout)
